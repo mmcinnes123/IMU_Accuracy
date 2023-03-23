2930210f3c9ce3ed864f4ef989305f056b96b280
mmcinnes123 <m.mcinnnes1.22@abdn.ac.uk>
2023-03-23 18:45:51 +0000

    Created Z-transformation script which simply multiplies the IMU quaternions at every time sample by a specified quaternion. This is doen to transform the orientations, such that they are expresse in a different global coordinate system. For example, the Delsys IMU's are expressed in a z-down, y-forward, x-left global coordinate system, and I transformed them to match OptiTrack's global Coordinate system: y-up, z-forward, x-right, by rotating all orientations by -90degrees around the x-axis.

--- main.py
+++ main.py
@@ -1,38 +1,110 @@
 import pandas as pd
 import numpy as np
-import quaternion
+from scipy.spatial.transform import Rotation as R
 
 input_file = "Raw_data.csv"
 
-# IMU Data
+# Define some quaternion functions
+
+def quaternion_multiply(Q0, Q1):
+    """
+    Multiplies two quaternions.
+    Input
+    :param Q0: A 4 element array containing the first quaternion (q01,q11,q21,q31)
+    :param Q1: A 4 element array containing the second quaternion (q02,q12,q22,q32)
+    Output
+    :return: A 4 element array containing the final quaternion (q03,q13,q23,q33)
+    """
+    w0 = Q0[0]
+    x0 = Q0[1]
+    y0 = Q0[2]
+    z0 = Q0[3]
+    w1 = Q1[0]
+    x1 = Q1[1]
+    y1 = Q1[2]
+    z1 = Q1[3]
+    # Computer the product of the two quaternions, term by term
+    Q0Q1_w = w0 * w1 - x0 * x1 - y0 * y1 - z0 * z1
+    Q0Q1_x = w0 * x1 + x0 * w1 + y0 * z1 - z0 * y1
+    Q0Q1_y = w0 * y1 - x0 * z1 + y0 * w1 + z0 * x1
+    Q0Q1_z = w0 * z1 + x0 * y1 - y0 * x1 + z0 * w1
+    # Create a 4 element array containing the final quaternion
+    final_quaternion = np.array([Q0Q1_w, Q0Q1_x, Q0Q1_y, Q0Q1_z])
+    return final_quaternion
+
+def quaternion_conjugate(Q0):
+    w0 = Q0[0]
+    x0 = Q0[1]
+    y0 = Q0[2]
+    z0 = Q0[3]
+    output_quaternion = np.array([w0, -x0, -y0, -z0])
+    return output_quaternion
+
+# Create two data frames from data file - IMU and OptiTrack
+with open(input_file, 'r') as file:
+    columns_IMU = ["IMU_Q0", "IMU_Q1", "IMU_Q2", "IMU_Q3"]
+    IMU_df = pd.read_csv(file, header=0, usecols=columns_IMU)
 
 with open(input_file, 'r') as file:
-    columns = ["IMU_Q0", "IMU_Q1", "IMU_Q2", "IMU_Q3"]
-    IMU_df = pd.read_csv(file, header=0, usecols=columns)
+    columns_Opt = ["Opt_Q0", "Opt_Q1", "Opt_Q2", "Opt_Q3"]
+    Opt_df = pd.read_csv(file, header=0, usecols=columns_Opt)
+
+quat_1 = np.array((IMU_df.values[0,:4]))
+quat_2 = np.array((IMU_df.values[0,0], IMU_df.values[1,1], IMU_df.values[1,2], IMU_df.values[1,3]))
+
 
 # Specify first quaternion as the reference orientation from which to measure all rotations
-IMU_ref = np.quaternion(IMU_df.values[0,0], IMU_df.values[0,1], IMU_df.values[0,2], IMU_df.values[0,3])
-IMU_ref_conj = np.conjugate(IMU_ref)
+IMU_ref = np.array((IMU_df.values[0,:4]))
+IMU_ref_conj = quaternion_conjugate(IMU_ref)
+
+Opt_ref = np.array((Opt_df.values[0,:4]))
+Opt_ref_conj = quaternion_conjugate(Opt_ref)
 
 
-# For every time sample, calculate the rotation quaterion, relative to the reference quat.
-# N = len(IMU_df.index)
-# a_1 = np.zeros((N, 4))
-# a = as_quat_array(a)
+# For every time sample, calculate the rotation quaterion, relative to the reference orientation (quat from first time sample).
 
-# for row in IMU_df:
-row = 0
-IMU_i = np.quaternion(IMU_df.values[row,0], IMU_df.values[row,1], IMU_df.values[row,2], IMU_df.values[row,3])
-IMU_rot = IMU_ref_conj*IMU_i
-a = IMU_rot
+N = len(IMU_df)
+IMU_rotations = np.zeros((N,4))
+Opt_rotations = np.zeros((N,4))
 
-row = 1
-IMU_i_2 = np.quaternion(IMU_df.values[row,0], IMU_df.values[row,1], IMU_df.values[row,2], IMU_df.values[row,3])
-IMU_rot_2 = IMU_ref_conj*IMU_i_2
-b = IMU_rot_2
+for row in range(len(IMU_df)):
+    IMU_i = np.array((IMU_df.values[row,:4]))
+    IMU_rotations[row] = quaternion_multiply(IMU_ref_conj,IMU_i)
 
-c = np.array([a,b])
-d = quaternion.as_float(a)
+for row in range(len(Opt_df)):
+    Opt_i = np.array((Opt_df.values[row,:4]))
+    Opt_rotations[row] = quaternion_multiply(Opt_ref_conj,Opt_i)
 
-print(c)
-print(d)
+
+# For every rotation quaternion pair (IMU and OptiTrack), calculate the difference in orientation between these two quaternions
+
+IMU_2_Opt_rot = np.zeros((N,4))
+
+for i in range(len(Opt_rotations)):
+    IMU_2_Opt_rot[i] = quaternion_multiply(quaternion_conjugate(Opt_rotations[i]), IMU_rotations[i])
+
+
+
+# r = R.from_quat(IMU_2_Opt_rot[0])
+# d = r.as_euler('xyz', degrees=True)
+# e = r.as_quat()
+# print(e)
+# print(d)
+
+print(type(IMU_2_Opt_rot))
+
+np.savetxt('Output_data.csv', IMU_2_Opt_rot, delimiter=',', fmt='%.6f')
+# with open(output_file, 'w') as file:
+#     IMU_2_Opt_rot.to_csv()
+
+
+#
+# IMU_2_Opt_rot = np.array(np.quaternion(1,0,0,0))
+#
+# for i in range(len(Opt_rotations)):
+#     a = np.conjugate(Opt_rotations[i])*IMU_rotations[i]
+#     IMU_2_Opt_rot = np.append(IMU_2_Opt_rot, a)
+#
+#     # Now need to output as Euler angles
+# b = list(Opt_df.values[0,:4])
+# print(b)
---
+++ z_transformation.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+input_file = "Raw_Data_from_Opti.csv"
+
+
+def quaternion_multiply(Q0, Q1):
+    """
+    Multiplies two quaternions.
+    Input
+    :param Q0: A 4 element array containing the first quaternion (q01,q11,q21,q31)
+    :param Q1: A 4 element array containing the second quaternion (q02,q12,q22,q32)
+    Output
+    :return: A 4 element array containing the final quaternion (q03,q13,q23,q33)
+    """
+    w0 = Q0[0]
+    x0 = Q0[1]
+    y0 = Q0[2]
+    z0 = Q0[3]
+    w1 = Q1[0]
+    x1 = Q1[1]
+    y1 = Q1[2]
+    z1 = Q1[3]
+    # Computer the product of the two quaternions, term by term
+    Q0Q1_w = w0 * w1 - x0 * x1 - y0 * y1 - z0 * z1
+    Q0Q1_x = w0 * x1 + x0 * w1 + y0 * z1 - z0 * y1
+    Q0Q1_y = w0 * y1 - x0 * z1 + y0 * w1 + z0 * x1
+    Q0Q1_z = w0 * z1 + x0 * y1 - y0 * x1 + z0 * w1
+    # Create a 4 element array containing the final quaternion
+    final_quaternion = np.array([Q0Q1_w, Q0Q1_x, Q0Q1_y, Q0Q1_z])
+    return final_quaternion
+
+
+# Define our multiplication quaternion
+# -90 degree rotation around x-axis:
+rot_quat = np.array([0.70711, -0.70711, 0, 0])
+
+# Read in both the IMU and OptiTrack data (OptiTrack data will be unchanged and re-written into output file)
+with open(input_file, 'r') as file:
+    columns_IMU = ["IMU_Q0", "IMU_Q1", "IMU_Q2", "IMU_Q3"]
+    IMU_df = pd.read_csv(file, header=10, usecols=columns_IMU)
+
+with open(input_file, 'r') as file:
+    columns_OpTr = ["OpTr_Q0", "OpTr_Q1", "OpTr_Q2", "OpTr_Q3"]
+    OpTr_df = pd.read_csv(file, header=10, usecols=columns_OpTr)
+
+N = len(IMU_df)
+transformed_quats = np.zeros((N, 4))
+
+for row in range(len(IMU_df)):
+    quat_i = np.array(IMU_df.values[row,:4])
+    transformed_quats[row] = quaternion_multiply(rot_quat, quat_i)
+
+# Turn OpTr data frame into a numpy array
+OpTr_array = OpTr_df.to_numpy()
+
+all_data = np.concatenate((transformed_quats, OpTr_array), axis=1)
+np.savetxt('Transformed_data.csv', all_data, delimiter=',', fmt='%.6f', header="IMU_Q0,IMU_Q1,IMU_Q2,IMU_Q3,OpTr_Q0, OpTr_Q1, OpTr_Q2, OpTr_Q3", comments='')
